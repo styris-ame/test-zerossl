9deb279a62630960b52690ea069448234c897a37
Styris <styris@styris.net>
2025-06-28 01:33:41 -0700

    Update main

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
 @echo off
 
 echo "%~dp0\test"
-set "PATH=%PATH%;%~dp0openssl\bin"
-set "OPENSSL_DIR=%~dp0openssl"
+
 set "API_KEY=3bcf584c39b4e48fbd5b82225805f6be"
 set "RUST_BACKTRACE=1"
-
+REM set "OPENSSL_DIR=%~dp0openssl"
+set "OPENSSL_STATIC=1"
+set "OPENSSL_LIB_DIR=%~dp0openssl\lib-win"
+set "OPENSSL_INCLUDE_DIR=%~dp0openssl\include-win"
 cargo test --workspace
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::io::Write;
+use zerossl::{generate_rsa_2048_priv_key, Client};
+
 fn main() {
-    // Code goes here
+    let test_domain = "dev.getlit.sh".to_string();
+    let is_ip = false;
+
+    let api_key = env::var("API_KEY").unwrap();
+    let client = Client::new(api_key);
+
+    println!("Breh1");
+    let pkey = generate_rsa_2048_priv_key().unwrap();
+
+    let mut domains: Vec<String> = Vec::new();
+    domains.push(test_domain.clone());
+    println!("Breh2");
+    let bytes = pkey.private_key_to_pem_pkcs8().unwrap();
+
+    println!("L RATIO");
+
+    let path = env::current_dir().unwrap();
+    println!("The current directory is {}", path.display());
+
+    let mut file = std::fs::File::create("./server.key").unwrap();
+    file.write_all(&bytes).unwrap();
+
+    println!("FINISH FILE WRITTEN");
 }
